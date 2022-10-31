55ca946b116337fdf8325c74296131526685181e
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-10-31 15:30:08 -0230

    Creation of Dynamic Array Class Practice

---
+++ dynamic-array.js
@@ -0,0 +1,30 @@
+class DynamicArray {
+
+  constructor(defaultSize=4) {
+
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(this.capacity)
+
+  }
+
+  read(index) {
+
+    if (this.data >= this.length) {
+      return undefined;
+    }
+    return this.data[index]
+  }
+
+  unshift(val) {
+
+    for (let i=this.length; i>0; i--){
+      this.data[i] = this.data[i-1]
+    }
+    this.data[0] = val
+    this.length++
+  }
+}
+
+
+module.exports = DynamicArray;
